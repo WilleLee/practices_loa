45a51a589e1b0acc16f0e0f5a099798457f84988
Wille Lee <1992season@gmail.com>
2023-10-20 15:06:39 +0900

    [REFACTOR] refactored to useCounter.ts

--- src/components/Counter/index.tsx
+++ src/components/Counter/index.tsx
@@ -1,19 +1,7 @@
-import { useState } from "react";
+import useCounter from "../../hooks/useCounter";
 
 const Counter = () => {
-  const [count, setCount] = useState(0);
-  const handleClickDecrement = () => {
-    setCount((prev) => {
-      if (prev === 0) return 0;
-      return prev - 1;
-    });
-  };
-  const handleClickIncrement = () => {
-    setCount((prev) => {
-      if (prev === 10) return 10;
-      return prev + 1;
-    });
-  };
+  const { count, handleClickIncrement, handleClickDecrement } = useCounter();
 
   return (
     <div>
